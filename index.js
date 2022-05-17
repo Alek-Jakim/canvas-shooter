4c00a3dcd781fbb6c2eb9117873a70eae2f2fe33
Alek <alek.jakimovski1@gmail.com>
2022-05-17 15:20:09 +0200

    create player

--- index.js
+++ index.js
@@ -5,4 +5,27 @@ const ctx = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-console.log(canvas)
+// Create player
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+};
+
+const x_coord = canvas.width / 2;
+const y_coord = canvas.height / 2;
+
+
+const player = new Player(x_coord, y_coord, 30, "#333");
+
+player.draw();
