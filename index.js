ef2bcdbce4dc18eec6b3bdf7173cb4a0dd14b57d
Alek <alek.jakimovski1@gmail.com>
2022-05-17 15:09:43 +0200

    project setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+
+// 1st thing is always set the canvas width & height
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+console.log(canvas)
